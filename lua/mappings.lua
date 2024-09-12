603613cd2b91c027d53d433ec2a39e74ab68fa65
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-09-12 10:26:12 +0200

    Added bind for todo comments in quickfix

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,7 @@ map("n", "<leader>gl", "<cmd> LazyGit <cr>", { desc = "Lazygit" })
 
 --TodoTelescope
 map("n", "<leader>ft", "<cmd> TodoTelescope <cr>", { desc = "TodoTelescope" })
+map("n", "<leader>fq", "<cmd> TodoQuickFix <cr>", { desc = "TodoQuickFix" })
 
 --Overseer
 map("n", "<leader>o", "<cmd> OverseerToggle left <cr>", { desc = "Toggle Overseer" })
