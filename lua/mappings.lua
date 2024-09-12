df11b05b5c20cf53c5652cdf887ca6282363b11a
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-09-12 13:02:40 +0200

    Added bind for overseer run

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,7 +15,8 @@ map("n", "<leader>ft", "<cmd> TodoTelescope <cr>", { desc = "TodoTelescope" })
 map("n", "<leader>fq", "<cmd> TodoQuickFix <cr>", { desc = "TodoQuickFix" })
 
 --Overseer
-map("n", "<leader>o", "<cmd> OverseerToggle left <cr>", { desc = "Toggle Overseer" })
+map("n", "<leader>oo", "<cmd> OverseerToggle left<cr>", { desc = "Toggle Overseer" })
+map("n", "<leader>or", "<cmd> OverseerRun <cr>", { desc = "Overseer Run" })
 
 --notify
 map("n", "<leader>fn", "<cmd> Telescope notify <cr>", { desc = "Telescope Notify" })
